90a997b41a206b556238d8227d7a0936bf1b0f74
brewslee <brewslee21@proton.me>
2026-03-23 14:14:06 +0100

    created utils.c

--- utils.c
+++ utils.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <sys/socket.h>
 
-struct packet {
-	uint8_t type
-}
+#define MAX_DATA_SIZE 1024
+
+typedef struct packet {
+	uint8_t type; // packet type
+	uint32_t seq; // sequence number
+	uint32_t data_len;
+	char data[MAX_DATA_SIZE];
+} packet_t;
+
+int send_data(int sock, packet_t packet, int *err);
+packet_t recv_data(int sock, int *err);
+
+char *extract_data(packet_t packet);
